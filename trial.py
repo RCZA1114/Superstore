f28af5a370658faa692d04cbff72bdc6edb15e70
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2024-03-05 09:04:29 +0800

    Update trial.py

--- trial.py
+++ trial.py
@@ -3,7 +3,39 @@ from datetime import timedelta
 import plotly.express as px
 from data import *
 
-df = pd.read_csv("superstore.csv")
+data = pd.read_csv("superstore.csv")
+
+select_measure = st.selectbox('Select Measure', ('Sales', 'Profit'))
+
+select_region = data["Region"].unique()
+selected_region = st.selectbox('Select Region', select_region)
+
+df = data[data["Region"] == selected_region]
+
+groupcountry = df.groupby('Country').sum()[select_measure]
+chart = px.bar(groupcountry)
 
 
+st.plotly_chart(chart, use_container_width=True)
+
+
+    #_____________________________________________________________
+select_country = df['Country'].unique()
+selected_country = st.selectbox('Select Country', select_country)
+
+dfr = df[df["Country"] == selected_country]
+
+groupregion = dfr.groupby('State').sum()[select_measure]
+chartregion = px.bar(groupregion)
+st.plotly_chart(chartregion, use_container_width=True)
+
+    #______________________________________________________________
+select_state = dfr['State'].unique()
+selected_state = st.selectbox('Select State', select_state)
+
+dfs = dfr[dfr["State"] == selected_state]
+
+groupcity = dfs.groupby('City').sum()[select_measure]
+chartcity = px.bar(groupcity)
+st.plotly_chart(chartcity, use_container_width=True)
 
