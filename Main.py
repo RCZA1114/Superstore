2b8800fd4758a784f105fc08811ce36745f7fd6d
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2024-03-05 10:50:02 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -20,24 +20,4 @@ def load_data():
 data = load_data()
 select_measure = st.selectbox('Select Measure', ('Sales', 'Profit'))
 
-select_segment = data['Region'].unique()
-    
-selected_segment = st.selectbox('Select Segment', select_segment)
 
-df = data[data['Segment'] == selected_segment]
-
-groupseg = df.groupby('Category').sum()[select_measure]
-chart = px.bar(groupseg)
-
-st.plotly_chart(chart, use_container_width=True)
-    
-    #___________________________________________________________________
-select_cat = data['Category'].unique()
-selected_cat = st.selectbox('Select Category', select_cat)
-
-dfc = df[df['Category'] == selected_cat]
-
-groupcat = dfc.groupby('Sub-Category').sum()[select_measure]
-chartc = px.bar(groupcat)
-
-st.plotly_chart(chartc, use_container_width=True)
