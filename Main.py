f76ae6ce3c9923c0a0e6d87876aa8b613c61015b
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2024-03-05 10:56:18 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,18 +11,67 @@ st.set_page_config(
 
 st.title("Dashboard")
 
-@st.cache_data
-def load_data():
-    data = pd.read_csv('superstore.csv')
-
-    return data
 
 
-data = load_data()
 
-select_segment = data["Segment"].unique()
-select_measure = st.selectbox('Select Measure', ('Sales', 'Profit'))
+data = superstore()
+select_area = st.selectbox('Select Area', ('Supplies', 'Countries'))
+
+if select_area == 'Countries':
+    select_measure = st.selectbox('Select Measure', ('Sales', 'Profit'))
+
+    select_region = data["Region"].unique()
+    selected_region = st.selectbox('Select Region', select_region)
+
+    df = data[data["Region"] == selected_region]
+
+    groupcountry = df.groupby('Country').sum()[select_measure]
+    chart = px.bar(groupcountry)
 
 
-selected_segment = st.selectbox('Select Segment', select_segment)
+    st.plotly_chart(chart, use_container_width=True)
 
+
+    #_____________________________________________________________
+    select_country = df['Country'].unique()
+    selected_country = st.selectbox('Select Country', select_country)
+
+    dfr = df[df["Country"] == selected_country]
+
+    groupregion = dfr.groupby('State').sum()[select_measure]
+    chartregion = px.bar(groupregion)
+    st.plotly_chart(chartregion, use_container_width=True)
+
+    #______________________________________________________________
+    select_state = dfr['State'].unique()
+    selected_state = st.selectbox('Select State', select_state)
+
+    dfs = dfr[dfr["State"] == selected_state]
+
+    groupcity = dfs.groupby('City').sum()[select_measure]
+    chartcity = px.bar(groupcity)
+    st.plotly_chart(chartcity, use_container_width=True)
+
+else:
+    select_measure = st.selectbox('Select Measure', ('Sales', 'Profit'))
+
+    select_segment = data["Segment"].unique()
+    selected_segment = st.selectbox('Select Segment', select_segment)
+
+    df = data[data["Segment"] == selected_segment]
+
+    groupseg = df.groupby('Category').sum()[select_measure]
+    chart = px.bar(groupseg)
+
+    st.plotly_chart(chart, use_container_width=True)
+    
+    #___________________________________________________________________
+    select_cat = data["Category"].unique()
+    selected_cat = st.selectbox('Select Category', select_cat)
+
+    dfc = df[df["Category"] == selected_cat]
+
+    groupcat = dfc.groupby('Sub-Category').sum()[select_measure]
+    chartc = px.bar(groupcat)
+
+    st.plotly_chart(chartc, use_container_width=True)
