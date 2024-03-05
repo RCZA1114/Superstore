effbb35982718a1b4c95eb52bf080004c4f1aca0
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2024-03-05 10:54:16 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,8 +18,10 @@ def load_data():
 
 
 data = load_data()
+
+select_segment = data["Segment"].unique()
 select_measure = st.selectbox('Select Measure', ('Sales', 'Profit'))
 
-select_segment = data["Segment "].unique()
+
 selected_segment = st.selectbox('Select Segment', select_segment)
 
