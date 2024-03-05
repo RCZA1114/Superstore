8455e5765e4d3ebd5bae567e743a991c97c629f3
Ramon Carlos Araneta <109441850+RCZA1114@users.noreply.github.com>
2024-03-05 10:51:40 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -23,4 +23,3 @@ select_measure = st.selectbox('Select Measure', ('Sales', 'Profit'))
 select_segment = data["Segment"].unique()
 selected_segment = st.selectbox('Select Segment', select_segment)
 
-df = data[data["Segment"] == selected_segment]
